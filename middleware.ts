a0d0d03ef6f14243925fc1e1468d8960ead8dcb1
TheMiranha <lxbmiranda@gmail.com>
2024-05-30 22:34:38 -0300

    feat: middleware redirect to https instead http

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/calendar(.*)",
@@ -17,6 +18,15 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
+  const currentEnv = process.env.NODE_ENV as string;
+  if (currentEnv === 'production' &&
+    req.headers.get("x-forwarded-proto") !== "https") {
+    return NextResponse.redirect(
+      `https://${req.headers.get('host')}${req.nextUrl.pathname}`,
+      301
+    );
+  }
+
   if (isProtectedRoute(req)) auth().protect();
 });
 
